4869fc8c944a8509c4edd8bbd9db63a6449a5a8e
SimpleBinary <40658215+SimpleBinary@users.noreply.github.com>
2020-02-24 18:22:01 +1000

    Redesign Extension
    Version 2.0!!!

--- popup.js
+++ popup.js
@@ -1,102 +1,106 @@
-
-document.querySelector("button").onclick = function copyToClipboard() {
-  // Create a dummy input to copy the string array inside it
-  var dummy = document.createElement("input");
-
-  // Add it to the document
-  document.body.appendChild(dummy);
-
-  // Set its ID
-  dummy.setAttribute("id", "dummy_id");
-
-  // Output the array into it
-  document.getElementById("dummy_id").value= document.querySelector("h1").textContent;
-
-  // Select it
-  dummy.select();
-
-  // Copy its contents
-  document.execCommand("copy");
-
-  // Remove it as its not needed anymore
-  document.body.removeChild(dummy);
+function isDarkColor(rgb) {
+    return Math.round((
+      parseInt(rgb[0], 10) * 299 +
+      parseInt(rgb[1], 10) * 587 +
+      parseInt(rgb[2], 10) * 114) / 1000
+    ) <= 140;
 }
+
+document.querySelector("h1").onclick = function copyToClipboard() {
+    let dummy = document.createElement("input");
+
+    document.body.appendChild(dummy);
+    dummy.setAttribute("id", "dummy_id")
+    document.getElementById("dummy_id").value = document.querySelector("h1").textContent;
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+
+    try {
+        document.getElementById("message").classList.remove("copy-animation");
+    } catch {}
+
+    void document.getElementById("message").offsetWidth; // Alows re-running of CSS Animation
+
+    document.getElementById("message").classList.add("copy-animation");
+
+}
+
 document.body.onload = function popup() {
-let r = Math.floor((Math.random() * 256));
-let g = Math.floor((Math.random() * 256));
-let b = Math.floor((Math.random() * 256));
-document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-let ir = 255 - r;
-let ig = 255 - g;
-let ib = 255 - b;
-let h = "";
-let e = "";
-let x = "";
-let Hexadecimal = [
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F"
-];
-let int = r;
-let result = "";
-while (int > 0) {
-    result = Hexadecimal[int % 16] + result.toString();
-    int = Math.floor(int / 16)
-}
-console.log(result.toString().length)
-if (result.toString().length == 2) {
-    h = result.toString();
-}
-if (result.toString().length == 1) {
-    h = "0" + result.toString();
-}
-if (result.toString().length == 0) {
-    h = "00";
-}
-int = g;
-result = "";
-while (int > 0) {
-    result = Hexadecimal[int % 16] + result.toString();
-    int = Math.floor(int / 16)
-}
-if (result.toString().length == 2) {
-    e = result.toString();
-}
-if (result.toString().length == 1) {
-    e = "0" + result.toString();
-}
-if (result.toString().length == 0) {
-    e = "00";
-}
-int = b;
-result = "";
-while (int > 0) {
-    result = Hexadecimal[int % 16] + result.toString();
-    int = Math.floor(int / 16)
-}
-if (result.toString().length == 2) {
-    x = result.toString();
-}
-if (result.toString().length == 1) {
-    x = "0" + result.toString();
-}
-if (result.toString().length == 0) {
-    x = "00";
-}
+    let r = Math.floor((Math.random() * 256));
+    let g = Math.floor((Math.random() * 256));
+    let b = Math.floor((Math.random() * 256));
+    document.body.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+    let h = "";
+    let e = "";
+    let x = "";
+    let Hexadecimal = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F"
+    ];
+    let int = r;
+    let result = "";
+    while (int > 0) {
+        result = Hexadecimal[int % 16] + result.toString();
+        int = Math.floor(int / 16)
+    }
+    if (result.toString().length == 2) {
+        h = result.toString();
+    }
+    if (result.toString().length == 1) {
+        h = "0" + result.toString();
+    }
+    if (result.toString().length == 0) {
+        h = "00";
+    }
+    int = g;
+    result = "";
+    while (int > 0) {
+        result = Hexadecimal[int % 16] + result.toString();
+        int = Math.floor(int / 16)
+    }
+    if (result.toString().length == 2) {
+        e = result.toString();
+    }
+    if (result.toString().length == 1) {
+        e = "0" + result.toString();
+    }
+    if (result.toString().length == 0) {
+        e = "00";
+    }
+    int = b;
+    result = "";
+    while (int > 0) {
+        result = Hexadecimal[int % 16] + result.toString();
+        int = Math.floor(int / 16)
+    }
+    if (result.toString().length == 2) {
+        x = result.toString();
+    }
+    if (result.toString().length == 1) {
+        x = "0" + result.toString();
+    }
+    if (result.toString().length == 0) {
+        x = "00";
+    }
 
-document.getElementById("heading").textContent = "#" + h + e + x;
-document.getElementById("heading").style.color = 'rgb(' + ir + ',' + ig + ',' + ib + ')';
+    document.getElementById("heading").textContent = "#" + h + e + x;
+
+    let inverted = isDarkColor([r, g, b]) ? "#ffffff" : "#000000";
+
+    document.getElementById("heading").style.color = inverted;
 }
